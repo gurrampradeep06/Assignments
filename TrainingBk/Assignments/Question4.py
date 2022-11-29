0aba4a06816ed32c800a798963d12a0c7cc87119
Pradeep.gurram <pradeep.gurram@techigai.io>
2022-11-29 13:28:33 +0530

    Committed few files to the branch

--- TrainingBk/Assignments/Question4.py
+++ TrainingBk/Assignments/Question4.py
@@ -1,11 +1,10 @@
 class Question4:
-    def multiples(self,inp1,inp2):
+    def multiples(self, inp1, inp2):
         output = list()
-        for i in range(inp1,inp2+1):
-            if(i % 7 == 0 and i % 5 != 0):
+        for i in range(inp1, inp2 + 1):
+            if i % 7 == 0 and i % 5 != 0:
                 output.append(i)
         return output
-
 
 
 print("enter the ranges: ")
@@ -14,8 +13,4 @@ inp1 = int(input())
 print("Enter range2 : ")
 inp2 = int(input())
 obj1 = Question4()
-print("The values which are divisible by 7 and multiples of 5 are :",obj1.multiples(inp1,inp2))
-
-
-
-
+print("The values which are divisible by 7 and multiples of 5 are :", obj1.multiples(inp1, inp2))
