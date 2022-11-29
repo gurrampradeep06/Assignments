cd5dccae388d72d8cea21114dab7a1dd8044a0f6
Pradeep.gurram <pradeep.gurram@techigai.io>
2022-11-29 11:57:09 +0530

    pushing the assignment 2 Area of the circle

---
+++ TrainingBk/Assignments/Question2.main.py
@@ -0,0 +1,7 @@
+from TrainingBK.Assignments.Question2 import Question2
+
+
+print("Enter the radius of the circle : ")
+radius = float(input())
+circle1 = Question2()
+print("Area of the circle is : ",circle1.areaOfCircle(radius))
---
+++ TrainingBk/Assignments/Question2.py
@@ -0,0 +1,4 @@
+class Question2:
+    def areaOfCircle(self,radius):
+        return (22/7)*((radius)**2);
+
