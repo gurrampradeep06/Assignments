8a9bb7f3e7a8c1d12de02f4ef4fae6a7df985bc2
Pradeep.gurram <pradeep.gurram@techigai.io>
2022-11-29 12:18:20 +0530

    pushing the Question 3 in the assignment

---
+++ TrainingBk/Assignments/Question3.py
@@ -0,0 +1,11 @@
+input = "123"
+
+print(int(input),"--> string converted to Int and the type is:",type(int(input)))
+print(float(input),"--> string converted to Int and the type is:",type(float(input)))
+
+input2 = 100
+print(float(input2),"--> Integer converted to float and the type is:",type(float(input2)))
+print(str(input2),"--> Integer converted to float and the type is:",type(str(input2)))
+
+
+# long and double data types are not supported in python3
